ffe68c295615224b4e638ae54ab32c8ae13ac626
Jack Ellis <jack.m.ellis@hotmail.co.uk>
2024-06-16 09:44:16 +0100

    feat: update descriptions

---
+++ data/libraries.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "Jpex",
+    url: "https://github.com/jpex-js/jpex",
+    description: `Jpex is a typescript library that intelligently injects dependencies at build time via babel.
+This library was a labour of love that I worked on and refined over the course of 6 years. I felt the inversion of control principal was underappreciated in the javascript community, and other DI solutions were often cumbersome and clumsy. I wanted to create a DI client that required no setup and was super intuitive to use.
+I also created several adapters to easily integrate jpex into React, Vue, and Node applications, and have used it in several projects over the years.`,
+  },
+  { name: "vue-hoc" },
+  { name: "vuenit" },
+  { name: "vue-inject" },
+  { name: "require-extension-hooks" },
+  { name: "capra" },
+];
--- data/projects.js
+++ data/projects.js
@@ -6,10 +6,14 @@ export default [
     image: "assets/projects/screens/nftx-screenshot-3.png",
     yearsActive: `2023 - 2024`,
     description: `NFTX is a platform that allows users to create and trade NFT index funds.
-After working with NFTX previously, I was brought back to help out the V3 platform.
+After working with NFTX previously, I was brought back to help create the v3 platform.
 This involved building out an entirely new app from scratch, while maintaining the existing platform (and supporting migrations between the two).
-The app was written in Typescript, Next.js, TailwindCSS, Wagmi, and Viem.
-I also rewrote the back-end layer in node.js, using miniservices, redis, and bullmq, and set up the supporting infrastructure.`,
+The app was written in Typescript, Next.js, and TailwindCSS, as well as Wagmi, and Viem for blockchain interactions.
+I soley created a new api server in node.js, using express, redis, bullMQ, and mongoDB.
+I employed event-driven-design and miniservices, and followed functional programming paradigms and onion architecture.
+An SDK created for the previous iteration was also completely rewritten and published to support v3.
+I had a lead role in backlog grooming, sprint planning, and other scrum activities.
+I also worked closely with the devops team to set up the supporting infrastructure.`,
     skills: [
       "ts",
       "node",
@@ -36,7 +40,9 @@ I also rewrote the back-end layer in node.js, using miniservices, redis, and bul
     image: "assets/projects/screens/floordao-screenshot-1.png",
     yearsActive: "2022",
     description: `FloorDAO is an NFT-Fi startup that aims to increase liquidity in the NFT space.
-  I was brought in to help guide the front-end infrastructure and build out the web app.`,
+  I was brought in to help guide the front-end infrastructure and build out the web app.
+  The app was written in Typescript using React, Next.js, and ether.js.
+  I was also closely involved with product managers with defining, refining, and planning sprint work.`,
     skills: ["ts", "next", "react", "tailwind", "web3", "fp"],
   },
   {
@@ -48,6 +54,8 @@ I also rewrote the back-end layer in node.js, using miniservices, redis, and bul
     image: "assets/projects/screens/yield-screenshot-1.png",
     yearsActive: "2022",
     description: `Yield is another new app by NFTX focused on token yield farming.
+  Yield was another app by NFTX and I was brought in to handle much of the same responsibilities as before.
+  The front-end app was written in Typescript using React, Next.js, and TailwindCSS, as well as ethers.js and wagmi.
   I was responsible for building the web app using Next.js, as well as a back-end in node.js.`,
     skills: [
       "ts",
@@ -73,9 +81,10 @@ I also rewrote the back-end layer in node.js, using miniservices, redis, and bul
     image: "assets/projects/screens/sns-screenshot-1.png",
     yearsActive: "2022",
     description: `Stop N Swop is a marketplace for retro video games.
-  I was responsible for building the front-end from scratch using Vue.js.
-  I fully built out the back-end in node.js, using microservices and event driven design; leveraging redis and bullmq.
-  I also managed the devops and infrastructure systems.`,
+  I created the front-end from scratch using Vue.js and Nuxt.
+  I built out the back-end in node.js, leveraging microservices and event-driven-design; using, redis, bullmq, and mongoDB/mongoose, as well as an express server layer.
+  The entire stack closely followed a functional programming paradigm, onion archtecture, and dependency injection.
+  I also set up and managed the devops and infrastructure with docker and kubernetes.`,
     skills: [
       "ts",
       "node",
@@ -103,7 +112,8 @@ I also rewrote the back-end layer in node.js, using miniservices, redis, and bul
     description: `NFTX is a platform that allows users to create and trade NFT index funds.
     I came on board at the early startup stage. I helped build out the front-end of the platform, written with React and Next.js, as well as advise on best practices, artchitecture, and technology choices.
 The app was written in Typescript, Next.js, and TailwindCSS. It also used several Web3 libraries including ethers.js to interact with the blockchain.
-I later returned to help create and maintain a back-end API layer in node.js, as well as a consumable SDK, and all of the underlying infrastructure.`,
+I later returned to help create a back-end API layer in node.js, using express and mongoDB. I also helped to set up the infrastructure and devops.
+I also single-handedly wrote and published a consumable SDK, and all of the underlying infrastructure.`,
     skills: [
       "ts",
       "node",
@@ -130,9 +140,10 @@ I later returned to help create and maintain a back-end API layer in node.js, as
     url: "https://claimer.com",
     image: "assets/projects/screens/claimer-screenshot-1.png",
     yearsActive: "2021",
-    description: `Claimer is a startup that helps businesses claim R&D tax credits through a web app.
-  I joined Claimer as lead front-end developer, building out the web app -  which required a complete rewrite of the codebase.
-  The app was built with React, TailwindCSS, React-Query, and Framer Motion.
+    description: `Claimer is a startup that helps businesses claim R&D tax credits.
+  I joined Claimer as a founding lead front-end developer, building out app web app -  which required a complete rewrite of the codebase.
+  The app was built with React, TailwindCSS, React-Query, and Framer Motion. I was responsible for choosing and progressing the tech stack, and creating the application architecture.
+  As part of a small team, I carried out code reviews and created a style guide on best c ofing practices.
   I also collaborated on a generic data-collection engine written across typescript and Kotlin.`,
     skills: [
       "ts",
@@ -161,10 +172,11 @@ I later returned to help create and maintain a back-end API layer in node.js, as
     image: "assets/projects/screens/godaddy-screenshot-1.png",
     yearsActive: "2017 - 2021",
     description: `Godaddy provide domain registration and web hosting across a global market.
-  I joined Godaddy as a lead front-end developer on the EMEA team, working with Typescript, React, and Redux.
+  I worked at Godaddy as a lead front-end developer on the EMEA team, working with Typescript, React, and Redux.
   I worked on multiple greenfield projects, including a large scale app-skinning system that served multiple countries and languages, and covered everything from storefronts to domain registration, to turnkey websites.
   I also worked with the team to create and maintain multiple Open Source libraries.
-  I was also responsible for technology research, developing application architecture, and creating coding standards and best practices.`,
+  I was also responsible for technology research, developing application architecture, and creating coding standards and best practices. I also spent time working on performance improvements across the codebase.
+  I took on several mentorships, was involved in training and onboarding of staff, and managed several sub-teams, including quarterly performance reviews.`,
     skills: [
       "js",
       "ts",
@@ -192,8 +204,10 @@ I later returned to help create and maintain a back-end API layer in node.js, as
     url: "https://itsstonbury.co.uk",
     image: "assets/projects/screens/its-screenshot-1.jpeg",
     yearsActive: "2017",
-    description: `ITS Stonbury is a startup software company that provides solutions for the water industry.
-    I led a small team to build out a new web and mobile app using Vue.js and Ionic, with a C# back-end.`,
+    description: `ITS Stonbury is a startup software company that provides solutions for the water construction industry.
+    I led a small team to build out a new web and mobile app using Vue.js and Ionic, with a C# back-end.
+    Development of the application was heavily user-led and I was regularly involved in user feedback meetings,
+    I was responsible for the front-end tech stack and keeping up with development trends.`,
     skills: [
       "js",
       ".net",
@@ -221,7 +235,8 @@ I later returned to help create and maintain a back-end API layer in node.js, as
     yearsActive: "2016",
     description: `Fretwell-Downing Hospitality was a company that provided catering management solutions.
     I worked on a greenfield project aimed at the Australian market, which we brought from concept to production in a year.
-    I was responsible for building out a new web app using Angular.js on the front-end, and C# on the back-end.`,
+    I was responsible for building out a new web app using Angular.js on the front-end, and C# on the back-end.
+    There was a heavy focus on Test Driven Devleopment, using NUnit and Jasmine/Karma, as well as collaboration and peer programming.`,
     skills: [
       "js",
       ".net",
@@ -245,8 +260,8 @@ I later returned to help create and maintain a back-end API layer in node.js, as
     image: "assets/projects/screens/sits-screenshot-1.png",
     yearsActive: "2013 - 2016",
     description: `Tribal Group is a company that provides software solutions for the education sector.
-  I worked on multiple large projects in the university admissions team, using javascript and Uniface.
-  I also served as the team's scrum master for most of my time there.`,
+  I worked on multiple large projects in the university admissions team, using Javascript and Uniface. This included creating a complex applicant deduplication system, and building a highly configurable workflow application for clearance.
+  I also served as the team's scrum master for most of my time there and became a member of the web standards group.`,
     skills: ["js", "html", "css", "uniface", "sql", "scrum"],
   },
 ];
